aaa17c5d41a356976ea602ac3ba9514a9e29dda3
lilf4t <fatima.mmm@hotmail.se>
2024-12-16 10:32:54 +0100

    dkk

--- GUI/patient_window.py
+++ GUI/patient_window.py
@@ -1,6 +1,6 @@
 # What can a patient do?
 # ----------------------
-# 1. Sign up for the health center by entering their patient ID (egentligen ska man skriva person nr men jag har gett den serial), first name, last name, gender (F, M or NB), address, phone nr, birthdate (registration date should be recorded) (can only sign up once, so patient id should be unique)
+# 1. Sign up for the health center by entering their  first name, last name, gender (F, M or NB), address, phone nr, birthdate (registration date should be recorded) (can only sign up once, so patient id should be unique)
 # 2. See all their info and edit the information except for Patient ID and registration date.
 # 3. Book an appointment for a specific doctor. They should see a list of all doctors, their specilization and visit cost. See the available days and times for visiting the doctor and then book the appointment. (booking can only be made on a friday of the week for the coming week, should be changeable) 
 # 4. View their medical record (see all diagnosis and prescription) for each previous visit.
